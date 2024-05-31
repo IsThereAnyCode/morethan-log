8a231515fbbbe81cc5b8b8b4cb48e8662e4ee27c
IsThereAnyCode <hjk7625@gmail.com>
2024-05-31 16:16:34 +0900

    update 2

--- site.config.js
+++ site.config.js
@@ -12,8 +12,8 @@ const CONFIG = {
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `Jaehyun's Resume`,
+      href: "https://isthereanycode.github.io/",
     },
   ],
   // blog setting (required)
